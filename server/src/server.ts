ebc561bb7184fd9c32a631201361a7ac83e6ac5f
Sergei Michkov <sergei_michkov@epam.com>
2020-08-04 01:12:03 +0400

    added files in request

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from 'express';
+import {createReadStream, readFileSync} from 'fs';
+import {Readable} from 'stream';
 
 const app = express();
 const port = 3000;
@@ -18,35 +20,35 @@ app.get('/api/posts', (req: Request, res: Response) => {
       id: 1,
       user: 'Name Surname',
       link: '@surname',
-      text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
+      text: `2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
       likes: 10
     },
     {
       id: 1,
       user: 'Name Surname',
       link: '@surname',
-      text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
+      text: `3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
       likes: 10
     },
     {
       id: 1,
       user: 'Name Surname',
       link: '@surname',
-      text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
+      text: `4Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
       likes: 10
     },
     {
       id: 1,
       user: 'Name Surname',
       link: '@surname',
-      text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
+      text: `5Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
       likes: 10
     },
     {
       id: 1,
       user: 'Name Surname',
       link: '@surname',
-      text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
+      text: `6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolore expedita facere minima necessitatibus nesciunt quo.`,
       likes: 10
     },
     {
@@ -472,6 +474,14 @@ app.get('/api/posts', (req: Request, res: Response) => {
   ];
   const answer = posts.splice(start, limit);
   res.json(answer);
+});
+
+app.get('/api/file', async (req: Request, res: Response) => {
+  const file = readFileSync('./src/documents/pdf.pdf');
+
+  res.contentType('application/pdf');
+
+  res.send(file);
 });
 
 app.listen(port, () => {
