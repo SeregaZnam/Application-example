3af4aac156003d4b21bc3a0872cd142d36a552b2
Sergei Michkov <sergei_michkov@epam.com>
2020-08-10 02:45:06 +0400

    added sign in

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,1 +1,4 @@
 export * from './window.service';
+export * from './auth.service';
+export * from './file-reader.service';
+export * from './post.service';
--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Post } from '../models';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,12 @@ export class PostService {
   constructor(private readonly http: HttpClient) {}
 
   getPosts(start: number, limit: number): Observable<Post[]> {
-    return this.http.get<Post[]>(`http://localhost:4200/api/posts?start=${start}&limit=${limit}`);
+    const url = environment.api + '/api/posts';
+    return this.http.get<Post[]>(url, {
+      params: {
+        start: start.toString(),
+        limit: limit.toString()
+      }
+    });
   }
 }
